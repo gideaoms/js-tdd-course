e86b08d6cf7df43afa3f8874342bde9de2187e72
Gideão Silva <gideaoms@gmail.com>
2018-04-20 15:56:57 -0300

    added test coverage

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const restOfDivision = (number, divise) => number % divise === 0;
+
+const Fizzbuzz = (number) => {
+  if (restOfDivision(number, 3) && restOfDivision(number, 5)) {
+    return 'FizzBuzz';
+  }
+  if (restOfDivision(number, 3)) {
+    return 'Fizz';
+  }
+  if (restOfDivision(number, 5)) {
+    return 'Buzz';
+  }
+  return number;
+};
+
+const foo = () => (true);
+
+export default Fizzbuzz;
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,8 +1,22 @@
 import { expect } from 'chai';
-import Fizzbuz from '../src/main';
+import Fizzbuzz from '../src/main';
 
-describe('Main', () => {
-  it('should return Fizz when multipe of 3', () => {
-    expect(Fizzbuz(3)).to.be.equal('Fizz')
+describe('Fizzbuzz', () => {
+  it('should return Fizz when multiple of 3', () => {
+    expect(Fizzbuzz(3)).to.be.equal('Fizz');
+    expect(Fizzbuzz(6)).to.be.equal('Fizz');
+  });
+
+  it('should return Buzz when multiple of 5', () => {
+    expect(Fizzbuzz(5)).to.be.equal('Buzz');
+    expect(Fizzbuzz(10)).to.be.equal('Buzz');
+  });
+
+  it('should return FizzBuzz when multiple of 3 and 5', () => {
+    expect(Fizzbuzz(15)).to.be.equal('FizzBuzz');
+  });
+
+  it('should return the number when non-multiple', () => {
+    expect(Fizzbuzz(7)).to.be.equal(7);
   });
 });
